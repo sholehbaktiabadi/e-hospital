1cef7e3db53935a53ae7f6ccc526e604e7f55e00
pergijuah <sholeh.abadi@widyawicara.com>
2021-07-04 15:33:52 +0700

    refactor(): configure env variables

--- src/config/env-loader.ts
+++ src/config/env-loader.ts
@@ -3,6 +3,8 @@ dotenv.config();
 
 export const DatabaseType = 'postgres';
 export const Env = () => ({
+  SERVER_ENV: +process.env.SERVER_ENV,
+  SERVER_PORT: +process.env.SERVER_PORT,
   DB_HOST: process.env.DB_HOST,
   DB_PORT: +process.env.DB_PORT,
   DB_NAME: process.env.DB_NAME,
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Env } from './config/env-loader';
 
 async function bootstrap() {
+  const { SERVER_PORT } = Env();
   const app = await NestFactory.create(AppModule);
-  await app.listen(1010);
+  await app.listen(SERVER_PORT);
 }
 bootstrap();
