e4d32249ac563a826d77c0d6f454da2e4221148c
pergijuah <sholeh.abadi@widyawicara.com>
2021-09-21 15:16:35 +0700

    change port unvariable

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const PORT = process.env.SERVER_PORT || 3000;
-  await app.listen(PORT, () => {
+  // const PORT = process.env.SERVER_PORT || 3000;
+  await app.listen(3000, () => {
     Logger.log(
       ' -----------------------------------------------',
       'Server Note',
@@ -16,7 +16,7 @@ async function bootstrap() {
       'Server Note',
     );
     Logger.log(
-      `|       Server running on port : ${PORT}           |`,
+      `|       Server running on port : 3000           |`,
       'Server Note',
     );
     Logger.log(
