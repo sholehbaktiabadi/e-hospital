15c62884c75d543ad87f6837273568fd34fd524a
pergijuah <sholeh.abadi@widyawicara.com>
2021-09-21 17:06:42 +0700

    change port

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   // const PORT = process.env.SERVER_PORT || 3000;
-  await app.listen(3000, '0.0.0.0');
+  const host = '0.0.0.0';
+  const port = process.env.PORT || 3000;
+  app.listen(port, host, function() {
+    console.log(`Server started.......${port}`);
+  });
   Logger.log('3000');
 }
 bootstrap();
