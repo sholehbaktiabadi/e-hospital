a466fb3e22ec4f74a2e0817865fff682bde1f685
pergijuah <sholeh.abadi@widyawicara.com>
2021-09-21 17:00:54 +0700

    change port

--- src/main.ts
+++ src/main.ts
@@ -6,27 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   // const PORT = process.env.SERVER_PORT || 3000;
-  await app.listen(3000, () => {
-    Logger.log(
-      ' -----------------------------------------------',
-      'Server Note',
-    );
-    Logger.log(
-      '|                                               |',
-      'Server Note',
-    );
-    Logger.log(
-      `|       Server running on port : 3000           |`,
-      'Server Note',
-    );
-    Logger.log(
-      '|                                               |',
-      'Server Note',
-    );
-    Logger.log(
-      ' -----------------------------------------------',
-      'Server Note',
-    );
-  });
+  await app.listen(3000, '0.0.0.0');
+  Logger.log('3000');
 }
 bootstrap();
