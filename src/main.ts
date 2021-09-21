a6bbaf98b4d3ec589988f9c9b9a4e3efb61f052e
pergijuah <sholeh.abadi@widyawicara.com>
2021-09-21 14:55:16 +0700

    enable cors access

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const PORT = process.env.SERVER_PORT || 3000;
   await app.listen(PORT, () => {
     Logger.log(
